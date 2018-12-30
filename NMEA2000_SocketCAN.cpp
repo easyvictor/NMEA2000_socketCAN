496fa09244e43c4c6b162d24325a7a0e513657c5
Jonathan Lovegren <44755365+easyvictor@users.noreply.github.com>
2018-12-30 18:56:16 -0500

    Create file if necessary for writing

--- NMEA2000_SocketCAN.cpp
+++ NMEA2000_SocketCAN.cpp
@@ -150,7 +150,7 @@ bool tNMEA2000_SocketCAN::CANGetFrame(unsigned long &id, unsigned char &len, uns
 //*****************************************************************************
 tSocketStream::tSocketStream(const char *_port) : port(-1) {
   if ( _port!=0 ) {
-    port=open(_port, O_RDWR | O_NOCTTY | O_NDELAY);
+    port=open(_port, O_RDWR | O_NOCTTY | O_NDELAY | O_CREAT);
   }
 
   if ( port!=-1 ) {
